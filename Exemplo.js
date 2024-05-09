ae0f2ccb87f36237c7a187ed123981ec12da2700
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-09 08:48:39 -0300

    feat: exercicio 1

---
+++ Exemplo.js
@@ -0,0 +1,42 @@
+//Apresenta uma mensagem na tela (sendo possível também o console.log)//
+// alert("Hello Word!!")
+
+// Escreva um programa que solicite o nome do usuário
+// e lhe de boas vindas, exibindo uma mensagem pelo console. 
+
+//prompt() -> recebe uma entrada digitada
+// let nome = prompt("Olá, digite o seu nome: ");
+
+// Concatenação -> juntar texto
+// console.log("Seja bem vindo" + nome + "!!!");
+
+// let pontos = 100;
+
+// let bonus = parseInt(prompt("Quantos pontos voce deseja somar?"));
+
+// Adição -> num1 + num2
+// Subtração -, Multi *, e Div / ...
+
+// let totalPontos = pontos + bonus;
+
+// console.log(totalPontos);
+
+// --- parseInt Transforma o que esta dentro de um () em numero inteiro: 1,2,3... ---
+
+//  let nota1 = parseInt(prompt("Digite uma nota: "))
+//  let nota2 = parseInt(prompt("Digite uoutra nota: "))
+
+// let media = nota1 + nota2 /2
+
+// console.log(media) 
+
+
+// parseFloat => Transforma o que esta entre () em numero com virgula / numero frasionado
+let produto1 = parseFloat(prompt("Digite o valor do produto"))
+
+let produto2 = parseFloat(prompt("Digite o valor do segundo produto"))
+
+let soma = produto1 + produto2;
+
+console.log(soma)
+
---
+++ ex1.js
@@ -0,0 +1,28 @@
+// Exercício 1: Verificação de login
+// Escreva um programa que solicita ao usuário um nome de usuário e uma senha. Verifique se o
+// nome de usuário é &quot;admin&quot; e a senha é &quot;senha123&quot;. Exiba uma mensagem indicando se o login
+// foi bem-sucedido ou não.
+
+
+
+// Solicita ao usuario o nome de usuario
+let username = prompt("Digite um nome de usuário: ")
+
+
+// Solicita  ao usuario uma senha.
+let senha = prompt("Digite a senha: ")
+
+
+
+// Verifique se o nome de usuario é "admin" E a senha é "senha123".
+// CASO SIM -> Exiba a msg de Login bem sucedida
+if ( username == "admin" && senha == "senha123"){
+    console.log("Login bem sucedido");
+} else {
+console.log("Voce nao tem permissão de acesso")
+}
+
+console.log("Bem vindo" + username)
+
+
+// Exiba uma mensagem indicando se o login foi bem sucedido ou nao.
