5de031af612d9dde0f506bb0f2fbc301a87601f4
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-10 09:39:43 -0300

    feat: exercicio 6

--- ex5.js
+++ ex5.js
@@ -3,17 +3,17 @@
 // crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou
 // não.
 
-let numero1 = parseInt(prompt("Digite o primeiro numero: "))
-let numero2 = parseInt(prompt("Digite o segundo numero: "))
-let numero3 = parseInt(prompt("Digite o terceiro numero: "))
+// let numero1 = parseInt(prompt("Digite o primeiro numero: "))
+// let numero2 = parseInt(prompt("Digite o segundo numero: "))
+// let numero3 = parseInt(prompt("Digite o terceiro numero: "))
 
-if (numero1 < numero2 && numero2 < numero3){
-    alert ("Crescente")
-    console.log ("Crescente")
-}
+// if (numero1 < numero2 && numero2 < numero3){
+//     alert ("Crescente")
+//     console.log ("Crescente")
+// }
 
-else {
-    alert("Ordem Incorreta")
-    console.log("Ordem Incorreta")
-}
+// else {
+//     alert("Ordem Incorreta")
+//     console.log("Ordem Incorreta")
+// }
 
---
+++ ex6.js
@@ -0,0 +1,15 @@
+// Exercício 6: Verificação de idade
+// Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
+// de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+let data = parseInt(prompt("Digite seu ano  de nascimento: "))
+
+if (data <= "2005"){
+    alert ("Maior de idade")
+    console.log ("Maior de idade")
+}
+
+else {
+    alert ("Menor de idade")
+    console.log ("Menor de idade")
+}
