914375ceee59be25a4502cdb7f8b8e6eac8d5ec8
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-10 08:51:03 -0300

    feat: exercicio 5

--- ex4.js
+++ ex4.js
@@ -4,24 +4,24 @@
 // notas entre 5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
 
 
-let nota1 = parseFloat(prompt("Digite uma nota: "))
-let nota2 = parseFloat(prompt("Digite outra nota: "))
-let nota3 = parseFloat(prompt("Digite outra nota: "))
-let nota4 = parseFloat(prompt("Digite outra nota: "))
+// let nota1 = parseFloat(prompt("Digite uma nota: "))
+// let nota2 = parseFloat(prompt("Digite outra nota: "))
+// let nota3 = parseFloat(prompt("Digite outra nota: "))
+// let nota4 = parseFloat(prompt("Digite outra nota: "))
 
-let media = (nota1 + nota2 + nota3 + nota4) /4
+// let media = (nota1 + nota2 + nota3 + nota4) /4
 
-if (media >= 7.0){
-    alert ("APROVADO")
-    console.log ("APROVADO")
-}
+// if (media >= 7.0){
+//     alert ("APROVADO")
+//     console.log ("APROVADO")
+// }
 
-else if (media >= 5.0 && media <= 7){
-    alert ("RECUPERAÇÃO")
-    console.log ("RECUPERAÇÃO")
-}
+// else if (media >= 5.0 && media <= 7){
+//     alert ("RECUPERAÇÃO")
+//     console.log ("RECUPERAÇÃO")
+// }
 
-else if (media <= 4 ){ 
-    alert ("REPROVADO")
-    console.log ("REPROVADO")
-}
+// else if (media <= 4 ){ 
+//     alert ("REPROVADO")
+//     console.log ("REPROVADO")
+// }
---
+++ ex5.js
@@ -0,0 +1,19 @@
+// Exercício 5: Verificação de números em ordem crescente
+// Escreva um programa que solicita três números ao usuário e verifica se eles estão em ordem
+// crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou
+// não.
+
+let numero1 = parseInt(prompt("Digite o primeiro numero: "))
+let numero2 = parseInt(prompt("Digite o segundo numero: "))
+let numero3 = parseInt(prompt("Digite o terceiro numero: "))
+
+if (numero1 < numero2 && numero2 < numero3){
+    alert ("Crescente")
+    console.log ("Crescente")
+}
+
+else {
+    alert("Ordem Incorreta")
+    console.log("Ordem Incorreta")
+}
+
