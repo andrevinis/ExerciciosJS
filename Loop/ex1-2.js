4d2f2d978f3b93321b7a71f7864dfa325dcd730c
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-14 10:01:33 -0300

    feat: exercicio 2-2

--- Loop/ex1-2.js
+++ Loop/ex1-2.js
@@ -1,6 +1,6 @@
-// 1. Imprimir números de 1 a 10 utilizando o loop for
+// // 1. Imprimir números de 1 a 10 utilizando o loop for
 
-for (let num = 0; num <= 10; num++) {
-    console.log(num);
+// for (let num = 0; num <= 10; num++) {
+//     console.log(num);
 
-}
+// }
