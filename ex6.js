71d5915f5ee00bf9704efe09b0918180b43ecbd6
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-14 09:39:39 -0300

    feat: exercicio 7

--- ex6.js
+++ ex6.js
@@ -2,14 +2,14 @@
 // Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
 // de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
 
-let data = parseInt(prompt("Digite seu ano  de nascimento: "))
+// let data = parseInt(prompt("Digite seu ano  de nascimento: "))
 
-if (data <= "2005"){
-    alert ("Maior de idade")
-    console.log ("Maior de idade")
-}
+// if (data <= "2005"){
+//     alert ("Maior de idade")
+//     console.log ("Maior de idade")
+// }
 
-else {
-    alert ("Menor de idade")
-    console.log ("Menor de idade")
-}
+// else {
+//     alert ("Menor de idade")
+//     console.log ("Menor de idade")
+// }
