0abc6cdf5586d281afc0fe62f6cee0365accf1e2
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-14 09:57:39 -0300

    feat: exercicio 1-2

---
+++ Loop/exemplo.js
@@ -0,0 +1,38 @@
+// for - loop contato
+
+// contando do 0 do 10
+
+// for (let num = 0; num <= 10; num++) {
+//     console.log(num);
+
+// }
+
+
+// // contagem regressiva 
+
+// for (let cont = 10; cont >=0; cont --){
+//     console.log(cont);
+// }
+
+
+
+// // while - loop condicional
+
+// let salario = 1000;
+
+// while(salario< 5000){
+//     // salario = salario + 100;
+//         salario +=100;
+
+//         console.log("O salário ainda é RS" + salario)
+// }
+
+
+// // do while-- loop condicional
+
+// let saldo = 1000;
+
+// do{
+//     console.log ("Esse é o saldo" + saldo);
+//     saldo += 100;
+// } while(saldo < 5000);
