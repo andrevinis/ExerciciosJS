f0b0be0daf1791451075564d3787eb62a93843e6
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-15 08:39:30 -0300

    feat: exercicio 4

--- Loop/ex1-2.js
+++ Loop/ex1-2.js
@@ -2,5 +2,8 @@
 
 // for (let num = 0; num <= 10; num++) {
 //     console.log(num);
+// }
 
+// for (let cont = 10; cont <= 0; num--) { ------------------------ Ordem decrescente
+//          console.log(cont);
 // }
