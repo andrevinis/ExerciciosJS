c4c9c98a3865ea62df05c1350ac3bd36d9752c79
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-15 09:50:27 -0300

    feat: exercicio 9

--- Loop/ex8-2.js
+++ Loop/ex8-2.js
@@ -1,12 +1,12 @@
-// 8. Faça um programa que receba a idade do usuário, enquanto a
-// resposta do usuário for menor que 18 anos continue a solicitar a
-// idade e mostre uma mensagem quando a idade do usuário for
-// maior que 18.
+// // 8. Faça um programa que receba a idade do usuário, enquanto a
+// // resposta do usuário for menor que 18 anos continue a solicitar a
+// // idade e mostre uma mensagem quando a idade do usuário for
+// // maior que 18.
 
-let idade = parseInt(prompt("Informe sua idade: "))
+// let idade = parseInt(prompt("Informe sua idade: "))
 
-while(idade <18){
-    idade = parseInt (prompt("Voce é menor de idade. Por favor, digite sua idade novamente: "))
-}
+// while(idade <18){
+//     idade = parseInt (prompt("Voce é menor de idade. Por favor, digite sua idade novamente: "))
+// }
 
-alert ("Voce é maior de idade. Sua idade é: " + idade)
+// alert ("Voce é maior de idade. Sua idade é: " + idade)
