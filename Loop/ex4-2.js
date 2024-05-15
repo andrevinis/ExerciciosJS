7e3a5be5914172ad76d32b8d2a4034b3075b3111
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-15 09:29:16 -0300

    feat: exercicio 5

--- Loop/ex4-2.js
+++ Loop/ex4-2.js
@@ -1,19 +1,19 @@
 // 4. Faça um programa que imprima na tela a tabuada de um número
 // qualquer escolhido pelo usuário até o 10.
 
-let numero = parseInt(prompt("Digite um número para ver na tabuada: "))
+// let numero = parseInt(prompt("Digite um número para ver na tabuada: "))
 
 
 
-if(!isNaN(numero)){
-    for(let cont = 1; cont <=10; cont ++){
-        console.log(numero + "x" + cont + "=" + (numero*cont));
-    }
-}else{
-    console.log("Número invalido. Por favor, digite um número válido.")
-}
+// if(!isNaN(numero)){
+//     for(let cont = 1; cont <=10; cont ++){
+//         console.log(numero + "x" + cont + "=" + (numero*cont));
+//     }
+// }else{
+//     console.log("Número invalido. Por favor, digite um número válido.")
+// }
 
 
-// console.log(isNaN("H")); // se for um numero ele Retorna verdadeiro 
-// console.log(isNaN(123)); // se for um numero ele Retorna falso 
+// // console.log(isNaN("H")); // se for um numero ele Retorna verdadeiro 
+// // console.log(isNaN(123)); // se for um numero ele Retorna falso 
 
