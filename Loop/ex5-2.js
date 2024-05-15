635fb8ab1da9c7b300b258d02a55a8e761ecdecd
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-15 09:34:21 -0300

    feat: exercicio 6

--- Loop/ex5-2.js
+++ Loop/ex5-2.js
@@ -1,21 +1,21 @@
-// 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+// // 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
 
-// inicializar os primeiros dois numeros da sequencia deFibonacci.
-let fib1 = 1;
-let fib2 = 2;
+// // inicializar os primeiros dois numeros da sequencia deFibonacci.
+// let fib1 = 1;
+// let fib2 = 2;
 
-//Imprime os primeiros dez numeros da sequencia deFibonacci
-console.log("Os primeiros dez numeros da sequencia de Fibonacci são: ")
-console.log(fib1);
-console.log(fib2);
+// //Imprime os primeiros dez numeros da sequencia deFibonacci
+// console.log("Os primeiros dez numeros da sequencia de Fibonacci são: ")
+// console.log(fib1);
+// console.log(fib2);
 
-//Intera para calcular e imprimir os proximos 8 numeros 
+// //Intera para calcular e imprimir os proximos 8 numeros 
 
-for(let i = 2; i < 10; i++){
-    let nextfib = fib1 + fib2;
-    console.log(nextfib);
+// for(let i = 2; i < 10; i++){
+//     let nextfib = fib1 + fib2;
+//     console.log(nextfib);
 
-    //Atualizar os valores da fib1 e fib2 para os proximos cálculos
-    fib1 = fib2;
-    fib2 = nextfib;
-}
+//     //Atualizar os valores da fib1 e fib2 para os proximos cálculos
+//     fib1 = fib2;
+//     fib2 = nextfib;
+// }
