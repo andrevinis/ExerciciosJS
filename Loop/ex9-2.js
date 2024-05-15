08ba12906e7c340ed45e33d6cd8c474815d930d9
André Vinícius Rodrigues Costa <56748619866@info.local>
2024-05-15 09:54:37 -0300

    feat: exercicio 10

--- Loop/ex9-2.js
+++ Loop/ex9-2.js
@@ -1,15 +1,15 @@
-// 9. Faça um programa que receba um número do usuário e mostre
-// no console a contagem regressiva até 0.
+// // 9. Faça um programa que receba um número do usuário e mostre
+// // no console a contagem regressiva até 0.
 
 
-let numero = parseInt(prompt("Digite um numero para a contagem regressiva: "))
+// let numero = parseInt(prompt("Digite um numero para a contagem regressiva: "))
 
-console.log("Contagem regressiva esta começando em: " + numero)
+// console.log("Contagem regressiva esta começando em: " + numero)
 
-while(numero >= 0){
-    console.log(numero)
-    numero--;
+// while(numero >= 0){
+//     console.log(numero)
+//     numero--;
 
-}
+// }
 
-console.log("Contagem regressiva finalizada! ")
+// console.log("Contagem regressiva finalizada! ")
